380a878a584a31ba0cb74434b620e4d372b46503
Akshay Khemaney <khemaney.a@etoncollege.org.uk>
2020-11-17 14:36:25 +0000

    Started the main console application.

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -7,10 +7,55 @@
 
 import Foundation
 
-print("Hello, World!")
-let a = 5
-print("The value of a is: \(a)")
-print("Hello World")
+func main() {
+    
+    let choice = getValue("Choose an Algorithm: \n\n\t1. Bubble Sort\n\t2. Merge Sort\n\t3. Quick Sort\n\t4. Insertion Sort\n\t5. Linear Search\n\t6. Binary Search")
+    
+    bubbleSort()
+    mergeSort()
+    quickSort()
+    insertionSort()
+    linearSearch()
+    binarySearch()
+}
+
+func getValue(_ prompt: String) -> Int {
+    print(prompt)
+    if let input = readLine() {
+        if let choice = Int(input){
+            return choice
+        }
+    }
+    
+    return 0
+}
+
+func bubbleSort(){
+    print("TODO")
+}
+
+func mergeSort() {
+    print("TODO")
+}
+
+func quickSort() {
+    print("TODO")
+}
+
+func insertionSort() {
+    print("TODO")
+}
+
+func linearSearch() {
+    print("TODO")
+}
+
+func binarySearch() {
+    print("TODO")
+}
+
+main()
+
 
 
 
